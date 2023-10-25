8675651a3b51c164c715ec92c74a8ae74b81cfaa
menobista <divyar@iucaa.in>
2023-10-25 18:53:56 +0530

    new

---
+++ get_data.py
@@ -0,0 +1,7 @@
+# array of webpages to refresh
+# search for listing and arrange by date of advertisement
+# put advertisement name, agency name, email and contact details
+# if possible send a mail to the agency by predecided text. at once
+# make it a cron or watch job
+
+
